9adc0f5b06e168db1a760859d197673ec531117f
f <f>
2022-04-20 16:56:02 -0500

    se crea pagina

---
+++ controlador.js
@@ -0,0 +1,8 @@
+let numeros=Array(1,2,3,4,5,6,7,8,9,10)
+console.log(numeros)
+console.log(numeros[7])
+let nombres=Array("juan","sara")
+let edad=Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)
+for(let i=0; i<nombres.length;i++){
+    console.log(nombres[i]),edad
+}
